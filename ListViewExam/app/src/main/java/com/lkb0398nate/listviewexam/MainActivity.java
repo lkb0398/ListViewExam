aafce2920cdb72a028aa725753cf1f9859ea46bc
lkb <lkb0398@nate.com>
2015-08-27 17:02:26 +0900

    두가지 아이템 표시

--- ListViewExam/app/src/main/java/com/lkb0398nate/listviewexam/MainActivity.java
+++ ListViewExam/app/src/main/java/com/lkb0398nate/listviewexam/MainActivity.java
@@ -3,16 +3,18 @@ package com.lkb0398nate.listviewexam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<String> mData;
-    private ArrayAdapter<String> mAdapter;
+    private List<Map<String, String>> mData;
+    private SimpleAdapter mAdapter;
     private ListView mListView;
 
     @Override
@@ -37,16 +39,24 @@ public class MainActivity extends AppCompatActivity {
         mData = new ArrayList<>();
 
         for (int i = 0; i <= 100; i++) {
-            mData.add("item " + i);
+            Map<String, String> data = new HashMap<>();
+            data.put("item", "Item" + i);
+            data.put("subitem", "subItem" + i);
+            mData.add(data);
         }
     }
 
     private void initAdapter() {
-        mAdapter = new ArrayAdapter<String>(getApplicationContext(),
-                android.R.layout.simple_list_item_1, mData);
+        mAdapter = new SimpleAdapter(getApplicationContext(), mData,
+                android.R.layout.simple_list_item_2, new String[] {
+                        "item", "subitem"
+                }, new int[] {
+                        android.R.id.text1, android.R.id.text2
+                });
     }
+
     private void initListView() {
-        mListView = (ListView)findViewById(R.id.list_view);
+        mListView = (ListView) findViewById(R.id.list_view);
         mListView.setAdapter(mAdapter);
     }
 }
