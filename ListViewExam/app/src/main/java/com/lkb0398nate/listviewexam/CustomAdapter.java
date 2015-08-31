e4fcc6a8432783fb751ab6cc8912b9f7a8bff334
lkb <lkb0398@nate.com>
2015-08-31 13:23:45 +0900

    2. Custom 어댑터 작성

--- ListViewExam/app/src/main/java/com/lkb0398nate/listviewexam/CustomAdapter.java
+++ ListViewExam/app/src/main/java/com/lkb0398nate/listviewexam/CustomAdapter.java
@@ -47,6 +47,8 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder viewHolder;
+
         // 1. 레이아웃 구성
 
 //        convertView 처음 로딩 될 때 초기화 과정을 거쳐서 viewHolder에 저장
@@ -54,12 +56,38 @@ public class CustomAdapter extends BaseAdapter {
 
             View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_peole, parent, false);
 
+            convertView.findViewById(R.id.mic_view);
+
+
+            // 뷰를 각 소스로 연결
+            ImageView image = (ImageView) convertView.findViewById(R.id.mic_view);
+            TextView name = (TextView) convertView.findViewById(R.id.inter_text);
+            TextView phoneNumber = (TextView) convertView.findViewById(R.id.answer_text);
+
+
+            // viewHolder 를 생성 후, 가져온 View를 연결
+            viewHolder = new ViewHolder();
+            viewHolder.image = image;
+            viewHolder.name = name;
+            viewHolder.phone = phoneNumber;
+
+            //ViewHolder 를 convertView의 태그로 저장.
+            convertView.setTag(viewHolder);
+
         } else {
             // convertView 가 다시 로딩 될떄는 viewHolder에서 꺼내와서 재사용
             // 이점 : 속도가 빠르다
+
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         // 2. 레이아웃 데이터를 바인딩
+
+        People people1 = mData.get(position);
+        People people = (People) getItem(position);
+        viewHolder.image.setImageResource(people.getImageResoutceId());
+        viewHolder.name.setText(people.getName());
+        viewHolder.phone.setText(people.getPhonenumber());
 
         // 3. 완성된 View를 리턴
 
