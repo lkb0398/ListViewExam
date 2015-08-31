ad4ab341f82f681f9e536d914ab974e1b9b5f425
lkb <lkb0398@nate.com>
2015-08-31 10:36:08 +0900

    Custom 어탭더 작성 1. Data 클래스 작성

---
+++ ListViewExam/app/src/main/java/com/lkb0398nate/listviewexam/People.java
@@ -0,0 +1,42 @@
+
+package com.lkb0398nate.listviewexam;
+
+/**
+ * Created by kb on 2015-08-31.
+ */
+public class People {
+    private String name;
+    private String phonenumber;
+    private int imageResoutceId;
+
+    public People(String name, String phonenumber, int imageResoutceId) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.imageResoutceId = imageResoutceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getImageResoutceId() {
+        return imageResoutceId;
+    }
+
+    public void setImageResoutceId(int imageResoutceId) {
+        this.imageResoutceId = imageResoutceId;
+    }
+}
+
